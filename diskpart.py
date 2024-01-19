ec7897670f4490436dbddad937e847d77431ab1f
nekotinix <62836642+nekotinix@users.noreply.github.com>
2024-01-20 00:39:46 +0300

    pre-release update

---
+++ diskpart.py
@@ -0,0 +1,173 @@
+﻿from symbol import comparison
+import pwinput
+import subprocess
+import os
+from GOST_34_11_2012 import Hash
+from fs import open_fs
+import keyring
+import psutil
+# #присвоение пути
+# vhd_path = "C:\\MyDrive"
+# #Присвоение размера  
+# vhd_size = "1024" # 1 GB
+# #надпись
+# label="test2"
+fs="ntfs"
+# letter="G"
+# #Cвободное место
+
+def free_space():
+    # Получение информации о всех дисках
+    disks = psutil.disk_partitions(all=True)
+    # Перебор всех дисков и вывод свободного места на каждом диске в гигабайтах
+    for disk in disks:
+        usage = psutil.disk_usage(disk.mountpoint)
+        free_space_gb = usage.free / (1024 ** 3)
+        ds=str(f"Диск {disk.device} ({disk.mountpoint}) - свободно {free_space_gb:.2f} ГБ")
+        print(ds)
+        if free_space_gb < 2.0:
+            print(f"Недостаточно места на диске {disk.device}")
+            
+            #Создание виртуального диска
+
+def create_virtual_disk(vhd_path,vhd_size,label,letter):
+    #Создание виртуального диска
+    diskpart_script_create = (
+        f'select vdisk file="{vhd_path}"\n'
+        f'create vdisk file="{vhd_path}" maximum={vhd_size}\n'
+        'attach vdisk\n'
+        'create partition primary\n'
+        f'format fs={fs} label={label} quick\n'
+        f'assign letter={letter}\n'
+        'exit\n'
+    )
+    #Запуск скрипта Diskpart
+    subprocess.run(['diskpart'], input=diskpart_script_create.encode(), check=True)
+    return(print("Виртуальный диск успешно создан "))
+#Подключение виртуального диска
+def attach_virtual_disk(vhd_path):
+    file_path = vhd_path
+
+    # желаемое расширение
+    extension = '.vhd'
+
+    # создаем новое имя файла с расширением
+    new_file_path = file_path + extension
+  
+    #переименовываем файл
+    os.rename(file_path, new_file_path)
+
+    #Подключение виртуального диска
+    attach_disk_script = (
+        f'select vdisk file="{new_file_path}"\n'
+        'attach vdisk \n'
+        'exit\n'
+    )
+
+    # Выполнение скрипта diskpart
+    subprocess.run(['diskpart'], input=attach_disk_script.encode(), check=True)
+#Подключение диска в режиме только чтение
+def attach_virtual_disk_read_only(vhd_path):
+    file_path = vhd_path
+
+    # желаемое расширение
+    extension = '.vhd'
+
+    # создаем новое имя файла с расширением
+    new_file_path = file_path + extension
+  
+    #переименовываем файл
+    os.rename(file_path, new_file_path)
+
+    #Подключение виртуального диска
+    attach_disk_script_readonly = (
+        f'select vdisk file="{new_file_path}"\n'
+        'attach vdisk readonly\n'
+        'exit\n'
+    )
+
+    # Выполнение скрипта diskpart
+    subprocess.run(['diskpart'], input=attach_disk_script_readonly.encode(), check=True)
+
+#Отключение виртуального диска
+def detach_virtual_disk(vhd_path2):
+    #Отключение виртуального диска
+    detach_disk_script = (
+        f'select vdisk file="{vhd_path2}"\n'
+        'detach vdisk\n'
+        'exit\n'
+    ) 
+
+    # Запуск скрипта diskpart
+    subprocess.run(['diskpart'], input=detach_disk_script.encode(), check=True)
+    # путь к файлу
+
+    # получаем название файла без расширения
+    filename, extension = os.path.splitext(vhd_path2)
+    print(filename)  # /path/to/file
+    os.rename(vhd_path2, filename)
+    print('Виртуальный диск отключен')
+
+def view_tree():
+    my_fs=open_fs('C:')
+    my_fs.tree()
+   
+    
+def get_pass():
+    pd=pwinput.pwinput(prompt='Пароль:',mask='·')
+    print(pd)
+
+
+def add_hash():
+    y=Hash('pass',256)
+    y1=Hash('pass',256)
+    print('Хэш',y)
+    #Присвоение хэша пароля в хранилище
+    keyring.set_password('system','test',y)
+    #Извлечение хэша из хранилища
+    password=keyring.get_password('system','test')
+    print(password)
+    y1=str(y1)
+    password=str(password)
+    if y1==password:
+        print('Пароль верный:')
+        print(y1)
+        print(password)
+    else:
+        print('Пароль неверный:')
+        print(y1)
+        print(password)
+free_space()
+def hash_comparisson(passw):
+#Извлечение хэша из хранилища
+    password=keyring.get_password('system','test')
+    y=Hash(passw,256)
+    y=str(y)
+    password=str(password)
+    if y==password:
+        print('Пароль верный:')
+        return True
+    else:
+        print('Пароль неверный:')
+        return False
+import os
+import shutil
+import datetime
+def backup(vhd_path,backup_folder):
+    
+    # Указываем путь к файлу vhd и папке для сохранения бэкапа
+    vhd_path = 'C:\\MyDrive'
+    backup_folder = 'C:\\back'
+
+    # Создаем папку для бэкапа, если ее нет
+    if not os.path.exists(backup_folder):
+        os.makedirs(backup_folder)
+
+    # Генерируем название файла бэкапа с использованием текущего времени
+    now = datetime.datetime.now()
+    backup_file_name = f'backup_{now.strftime("%Y-%m-%d_%H-%M-%S")}'
+    backup_file_path = os.path.join(backup_folder, backup_file_name)
+
+    # Копируем файл vhd в папку бэкапа с новым именем
+    shutil.copy2(vhd_path, backup_file_path)
+    print(f'Создан бэкап файла {vhd_path} в {backup_file_path}')
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-﻿import customtkinter
+﻿from contextlib import closing
+import customtkinter
 import os
 from PIL import Image
-
-
+import diskpart
+from tkinter import filedialog
+path_vdisks=[]
+extension=".vhd"
+print('path_diskov',path_vdisks)
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -10,12 +14,11 @@ class App(customtkinter.CTk):
         self.title("Шифрование Виртуальных Дисков")
         self.geometry("700x450")
         self.iconbitmap('x_icon.ico')
-
-        # set grid layout 1x2
+        # Уставновка вывода 1x2
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
-        # load images with light and dark mode image
+        # подгрузка тем
         image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_images")
         self.logo_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "CustomTkinter_logo_single.png")), size=(26, 26))
         self.large_test_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "large_test_image.png")), size=(500, 150))
@@ -27,7 +30,7 @@ class App(customtkinter.CTk):
         self.add_user_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "add_user_dark.png")),
                                                      dark_image=Image.open(os.path.join(image_path, "add_user_light.png")), size=(20, 20))
 
-        # create navigation frame
+        # создание гланого окна
         self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
         self.navigation_frame.grid(row=0, column=0, sticky="nsew")
         # количество строк в разделе меню
@@ -66,37 +69,60 @@ class App(customtkinter.CTk):
         self.home_frame_large_image_label = customtkinter.CTkLabel(self.home_frame,text='')
         self.home_frame_large_image_label.grid(row=0, column=0, padx=20, pady=20)
 
-        self.home_frame_button_1 = customtkinter.CTkButton(self.home_frame, text="Создание виртуального диска")
+        self.home_frame_button_1 = customtkinter.CTkButton(self.home_frame, text="Создание виртуального диска",command=self.open_input_dialog_event)
         self.home_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
         self.checkbox_1=customtkinter.CTkCheckBox(self.home_frame,text="только для чтения")
         self.checkbox_1.grid(row=2, column=1, padx=20, pady=10)
-        self.home_frame_button_2 = customtkinter.CTkButton(self.home_frame, text="Подключение виртуального диска")
+        self.home_frame_button_2 = customtkinter.CTkButton(self.home_frame, text="Подключение виртуального диска",command=self.add_disk_button_event)
         self.home_frame_button_2.grid(row=2, column=0, padx=20, pady=10)
-        self.home_frame_button_3 = customtkinter.CTkButton(self.home_frame, text="Отключение виртуального диска")
+        self.home_frame_button_3 = customtkinter.CTkButton(self.home_frame, text="Отключение виртуального диска",command=self.remove_disk_event)
         self.home_frame_button_3.grid(row=3, column=0, padx=20, pady=10)
         self.home_frame_button_4 = customtkinter.CTkButton(self.home_frame, text="Автомонтирование")
         self.home_frame_button_4.grid(row=4, column=0, padx=20, pady=10)
         self.home_frame_text_box1=customtkinter.CTkTextbox(self.home_frame,height=160, width=400)
         self.home_frame_text_box1.grid(row=5, column=0,padx=20,pady=20)
-        #self.home_frame_text_box1.inse
-        # create second frame
+        # self.home_frame_text_box1.insert('0.1',)
+        # Создание второго подменю
         self.second_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
-        self.secondframe_textlb_1=customtkinter.CTkTextbox(self.second_frame,width=250,height=250)
-        self.secondframe_textlb_1.grid(row=0, column=2, padx=(20, 20), pady=(20, 20), sticky="nsew")
-        # create third frame
+        self.second_frame.grid_columnconfigure(0, weight=1)
+        self.second_frame_image_label = customtkinter.CTkLabel(self.second_frame,text='')
+        self.second_frame_image_label.grid(row=0, column=0, padx=20, pady=20)
+        # self.secondframe_textlb_1=customtkinter.CTkTextbox(self.second_frame,width=250,height=250)
+        # self.secondframe_textlb_1.grid(row=0, column=2, padx=(20, 20), pady=(20, 20), sticky="nsew")
+        self.second_frame_button1=customtkinter.CTkButton(self.second_frame,text='Зашифровать файл')
+        self.second_frame_button1.grid(row=1,column=0,padx=20,pady=10)
+        self.second_frame_button2=customtkinter.CTkButton(self.second_frame,text='Расшифровать файл')
+        self.second_frame_button2.grid(row=2,column=0,padx=20,pady=10)
+        self.second_frame_text_box1=customtkinter.CTkTextbox(self.second_frame,height=160, width=400)
+        self.second_frame_text_box1.grid(row=5, column=0,padx=20,pady=20)
+        # Создание 3 подменю
         self.third_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.third_frame.grid_columnconfigure(0, weight=1)
+        self.third_frame_image_label = customtkinter.CTkLabel(self.third_frame,text='')
+        self.third_frame_image_label.grid(row=0, column=0, padx=20, pady=20)
+        self.third_frame_button1=customtkinter.CTkButton(self.third_frame,text='Создать Бэкап',command=self.create_backup_event)
+        self.third_frame_button1.grid(row=1,column=0,padx=20,pady=10)
+        self.third_frame_option_menu_1=customtkinter.CTkOptionMenu(self.third_frame, values=["NUL"],
+                                                                )
+        self.third_frame_option_menu_1.grid(row=1, column=1, padx=20, pady=10, sticky="s")
+        self.third_frame_button2=customtkinter.CTkButton(self.third_frame,text='Перейти к папке',command=self.open_file_dialog)
+        self.third_frame_button2.grid(row=2,column=0,padx=20,pady=10)
+        self.third_frame_text_box1=customtkinter.CTkTextbox(self.third_frame,height=160, width=400)
+        self.third_frame_text_box1.grid(row=5, column=0,padx=20,pady=20)
         self.fourth_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
-        # select default frame
+        
+        
+        # Выбор подменю по молчанию
         self.select_frame_by_name("home")
 
     def select_frame_by_name(self, name):
-        # set button color for selected button
+        # установка цвета кнопки при наведении на неё
         self.home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
         self.frame_2_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
         self.frame_3_button.configure(fg_color=("gray75", "gray25") if name == "frame_3" else "transparent")
         self.frame_4_button.configure(fg_color=("gray75", "gray25") if name == "frame_4" else "transparent")
 
-        # show selected frame
+        # Показать выбранную кнопку
         if name == "home":
             self.home_frame.grid(row=0, column=1, sticky="nsew")
         else:
@@ -107,11 +133,14 @@ class App(customtkinter.CTk):
             self.second_frame.grid_forget()
         if name == "frame_3":
             self.third_frame.grid(row=0, column=1, sticky="nsew")
-        if name == "frame_4":
-            self.third_frame.grid(row=0, column=1, sticky="nsew")    
         else:
-            self.third_frame.grid_forget()
+            self.third_frame.grid_forget()    
+        if name == "frame_4":
+            self.fourth_frame.grid(row=0, column=1, sticky="nsew")    
+        else:
+            self.fourth_frame.grid_forget()
 
+    #Призвести действия заданные по кнопке
     def home_button_event(self):
         self.select_frame_by_name("home")
 
@@ -126,9 +155,80 @@ class App(customtkinter.CTk):
         
     def change_appearance_mode_event(self, new_appearance_mode):
         customtkinter.set_appearance_mode(new_appearance_mode)
+    def open_input_dialog_event(self):
+        dialog = customtkinter.CTkInputDialog(text="Введите путь хранения диска", title="Путь хранения")
+        print("CTkInputDialog:", dialog.get_input())
+        if dialog.get_input== None:
+            print('Введите значение')
+        dialog1=customtkinter.CTkInputDialog(text="Введите размер в Гб", title="Размер")
+        print("CTkInputDialog:", dialog1.get_input())
+        dialog2=customtkinter.CTkInputDialog(text="Введите имя подключения диска", title="Имя диска")
+        print("CTkInputDialog:", dialog2.get_input())
+        dialog3=customtkinter.CTkInputDialog(text="Введите пароль", title="пароль")
+        print("CTkInputDialog:",dialog3.get_input())
+        
+        
+    def add_disk_button_event(self):
+        if self.checkbox_1.get() ==1:
+            # dialog1=customtkinter.CTkInputDialog(text="Введите путь", title="Путь")
+            # vdisk_path=dialog1.get_input()
+            filename = filedialog.askopenfilename()
+            filename=filename.replace("/",'\\')
+            path_vdisks.append(filename+extension)
+            dialog3=customtkinter.CTkInputDialog(text="Введите пароль", title="пароль")
+            passw=dialog3.get_input()
+            if diskpart.hash_comparisson(passw) == True:
+                self.home_frame_text_box1.insert('0.1' ,'Пароль верный\n')
+                x=diskpart.attach_virtual_disk_read_only(filename)
+                print(filename)
+                self.home_frame_text_box1.insert('0.1' ,f'Диск  {filename} подключен в режиме чтения\n')
+            else:
+                self.home_frame_text_box1.insert('0.1' ,'Пароль неверный\n')
+            
+        elif self.checkbox_1.get()==0:
+           
+            # dialog1=customtkinter.CTkInputDialog(text="Введите путь", title="Путь")
+            # vdisk_path=dialog1.get_input()
+            filename = filedialog.askopenfilename()
+            filename=filename.replace("/",'\\')
+            path_vdisks.append(filename+extension)
+            print('pvd',path_vdisks)
+            dialog3=customtkinter.CTkInputDialog(text="Введите пароль", title="пароль")
+            passw=dialog3.get_input()
+            if diskpart.hash_comparisson(passw) == True:
+                self.home_frame_text_box1.insert('0.1' ,'Пароль верный\n')
+                diskpart.attach_virtual_disk(filename)
+                print(filename)
+                self.home_frame_text_box1.insert('0.1' ,f'Диск{filename} Подключен\n')
+            else:
+                self.home_frame_text_box1.insert('0.1' ,'Пароль неверный\n')   
+    def remove_disk_event(self):
+        # dialog1=customtkinter.CTkInputDialog(text="Введите путь", title="Путь")
+        filename = filedialog.askopenfilename()
+        filename=filename.replace("/",'\\')
+        diskpart.detach_virtual_disk(filename)
+        self.home_frame_text_box1.insert('0.1' ,f'Диск {filename} Отключен\n')
+    def create_backup_event(self):
+        dialog1=customtkinter.CTkInputDialog(text="Введите путь виртуального диска", title="Путь к виртуальному диску")
+        vhd_path=dialog1.get_input()
+        dialog2=customtkinter.CTkInputDialog(text="Введите путь сохранения бэкапа", title="Путь к сохрание бэкапа")
+        backup_path=dialog2.get_input()
+        d= diskpart.backup(vhd_path,backup_path)
+        success=str(f'Создан бэкап файла {vhd_path} в {backup_path}\n')                   
+        self.third_frame_text_box1.insert('0.1' ,success ,'\n')
+    def open_file_dialog(self):
+    # Открываем окно выбора файла
+        filename = filedialog.askopenfilename()
+    # Выводим путь к выбранному файлу на customtkinter
+        self.third_frame_text_box1.insert('0.1',filename)
+def on_closing():
+    for disk in range(len(path_vdisks)):
+        if len(path_vdisks) !=0:
+            diskpart.detach_virtual_disk(path_vdisks[disk])
+    app.destroy()       
 
-
+    
 if __name__ == "__main__":
     app = App()
+    #app.protocol("WM_DELETE_WINDOW",on_closing)
     app.mainloop()
-
